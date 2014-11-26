a7a8ee751f60bd9d7542ca38098d4b4c08af68fd
lw222gz_1 <lw222gz@student.lnu.se>
2014-11-26 21:49:23 +0000

    got the game mechanics to work, now only all the css and text such as showing the amount of tries they make etc.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -4,6 +4,8 @@ window.onload = function(){
     var picArray = [];
     var playboard = document.getElementById("playboard")
     var i;
+    var Ids = [];
+    var j = 0;
     var TileArray = [];
     var EightTiles = document.getElementById("4x2");
     var SixteenTiles = document.getElementById("4x4");
@@ -11,8 +13,10 @@ window.onload = function(){
     for (i = 0; i < 16; i += 1)
     {
         TileArray[i] = document.createElement("img");
+        Ids[i] = i;
         TileArray[i].src = "pics/0.png";
     }
+    
     
     for (i = 1; i < 16; i +=1)
     {
@@ -26,12 +30,39 @@ window.onload = function(){
         }
     }
     
+    /*         // TEST ZON! ska bara ha effekt när man refreshar    
+    var holder = function(){  
+        TileArray[0].src = "pics/1.png";        
+        var ja;
+        ja += 1;
+        console.log(ja);
+    }
+    playboard.appendChild(TileArray[0])
+   
+                 // ger undefined is not a function.
+    TileArray[0].addEventListener("click", function(e){
+        TileArray[0].src = "pics/1.png"; 
+    })      // varför skrer funktionen direkt?
+    
+    
+    
+    
+    
+        TileArray[0].removeEventListener("click", function(e){ // funkar endåså inte???? måste göra något fel
+        TileArray[0].src = "pics/1.png"; 
+    });
+        console.log("Damn")
+    
+     */           // TEST ZON!
     
     
     
     EightTiles.addEventListener("click", function(e){
         
+        
         resetAllTiles();
+        resetIds();
+        console.log(Ids);
         
         if (playboard.childNodes.length > 9)
         {
@@ -55,6 +86,8 @@ window.onload = function(){
     SixteenTiles.addEventListener("click", function(e) {
         
         resetAllTiles();
+        resetIds()
+        console.log(Ids);
         
         picArray = RandomGenerator.getPictureArray(4,4);
         
@@ -71,31 +104,76 @@ window.onload = function(){
     
     
     var check = 0;
+    var counter = 0;
+    var a = 0;
+    var b = 0;
     
     
     
     var addEvent = function(index) {
         TileArray[index].addEventListener("click", function(e){
-            TileArray[index].src = "pics/"+picArray[index]+".png";
-            pressedTile(index);
-            
+            if (Ids[index] === index)                               // kunde inte jämnföra img.src så gav alla bilder ett id som ges tillbaka rätt värde om man inte 
+            {
+                Ids[index] += 1;
+                TileArray[index].src = "pics/"+picArray[index]+".png";
+                pressedTile(index)
+            }
         })
     }
+    
+    
     
     var pressedTile = function(index){
          
          check += 1;
          
         
-        TileArray[index].removeEventListener("click", function(e){
-            TileArray[index].src = "pics/"+picArray[index]+".png";;
-        })
-        
         if (check == 2)
          {
              check = 0;
-             alert("HAI!")
+             a = index;
+             comparePictures(a,b)
          }
+         b = index;
+    }
+    
+    var comparePictures = function(a, b){
+        // ge +1 på count variabel som håller koll på antal försök.
+        counter += 1; // !! Lägg till så att detta demonstreras på hemsidan
+        
+        if (picArray[a] === picArray[b])
+        {
+            console.log("WIN!") //lägg till ett + för antal par man hittar av antalet som finns kvar att hitta.
+        }
+        
+        else
+        {
+            console.log("LOOSE!!")
+            Ids[a] -= 1;
+            Ids[b] -= 1;
+            for (i = 0; i < Ids.length; i += 1)
+            {
+                Ids[i] += 1;
+            }
+            
+            setTimeout(function() {
+                TileArray[a].src = "pics/0.png"
+                TileArray[b].src = "pics/0.png"
+                for (i = 0; i < Ids.length; i += 1)
+                {
+                    Ids[i] -=1;
+                }
+            }, 1000);
+            
+        }
+    }
+    
+    
+    var resetIds = function(){
+        for (i = 0; i < Ids.length; i += 1)
+        {
+            Ids[i] = i;
+        }
     }
     
     var clearBoard = function(){
