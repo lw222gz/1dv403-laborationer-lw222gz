f87017a8d8b8a3a3614d97ec9a2ec5cbe5c894c7
lw222gz_1 <lw222gz@student.lnu.se>
2014-11-30 15:42:42 +0000

    the game aspects work, just need to add the presentations.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,16 +2,15 @@
 
 var memory = {
     
-    
-    
+    Tries: 0,
+    PairsFound: 0,
     Ids: 0,
     picArray:null,
-    
-    
-    Cell: null,
+    check: true,
+    firstClick: null,
+    TurnedOver: 0,
     rows: 4,
     cols: 4,
-    StartGame: document.getElementById("4x4"),
     
     init:function() {
         memory.picArray = RandomGenerator.getPictureArray(memory.rows, memory.cols)
@@ -36,18 +35,17 @@ var memory = {
                 
                 aTag.href = "#"
                 
-                
-                
-                
                 img.src = "pics/0.png"
                 img.className = "pics/"+memory.picArray[memory.Ids]+".png"
                 aTag.img = img;
                 
-                aTag.addEventListener("click", memory.test)
+                memory.Ids += 1;
+                
+                aTag.addEventListener("click", memory.onClick)
                 
                 aTag.appendChild(img);
                 Cell.appendChild(aTag);
-                TableRow.appendChild(memory.Cell);
+                TableRow.appendChild(Cell);
             }
             table.appendChild(TableRow);
         }
@@ -56,11 +54,45 @@ var memory = {
     },
     
     
-    test: function() {
-       console.log(aTag.img.className)
+    onClick: function() {
+       
+       
+       if (memory.check === true)
+       {
+       memory.TurnedOver += 1;
+       this.img.src = this.img.className;
+       this.removeEventListener("click", memory.onClick);
+       
+           if (memory.TurnedOver === 2)
+           {
+               memory.check = false;
+               memory.TurnedOver = 0;
+               var secondClick = this;
+               if (memory.firstClick.img.className === secondClick.img.className)
+               {
+                   
+                   memory.check = true;
+               }
+               else
+               {
+                   
+                   setTimeout(function(){
+                       memory.firstClick.img.src = "pics/0.png";
+                       secondClick.img.src = "pics/0.png";
+                       memory.firstClick.addEventListener("click", memory.onClick);
+                       secondClick.addEventListener("click", memory.onClick);
+                       memory.check = true;
+                   }, 1000)
+               }
+               
+           }
+       }
+       if (memory.TurnedOver === 1)
+       {
+            memory.firstClick = this;
+       }
        
     },
-    
 }  
 
 window.onload = memory.init;
